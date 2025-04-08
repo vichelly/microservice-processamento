c4fda6c90dc07e3d15da58c2bf5fadfdea9ad1e9
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2025-04-07 21:45:24 -0300

    feat: final

--- src/main/java/meu/microservico/processamento/consumer/ProcessamentoConsumer.java
+++ src/main/java/meu/microservico/processamento/consumer/ProcessamentoConsumer.java
@@ -4,14 +4,12 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-import meu.microservico.processamento.dto.PedidoDTO;
-
 @Component
 public class ProcessamentoConsumer {
     
     @RabbitListener(queues = "${broker.queue.processamento.name}")
-    public void listernerProcessamentoQueue(PedidoDTO pedidoDTO){
-        System.out.println(pedidoDTO.getDescricao());
+    public void listernerProcessamentoQueue(@Payload String descricao){
+        System.out.println(descricao);
     }
 
 }
